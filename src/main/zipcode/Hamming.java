2b7bc945138703e5d20998302ca865339bd322d2
Danny Cao <dannycao1997@yahoo.com>
2024-04-09 16:07:42 -0400

    completed code for instance fields and constructor / passed 2 test cases

--- src/main/zipcode/Hamming.java
+++ src/main/zipcode/Hamming.java
@@ -1,7 +1,15 @@
 package zipcode;
 //
 public class Hamming {
+    String s;
+    String s1;
     public Hamming(String s, String s1) {
+        if (s.length() != s1.length()) {
+            throw new IllegalArgumentException("leftStrand and rightStrand must be of equal length.");
+        } else {
+            this.s = s;
+            this.s1 = s1;
+        }
     }
 
     public int getHammingDistance() {
